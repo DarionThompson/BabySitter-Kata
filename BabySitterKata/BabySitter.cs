c35b8b7b6e5a46d5a145131e3957e136c603700f
Darion Thompson <user@Darions-iMac.local>
2018-12-16 04:11:46 -0500

    refactored : added an aditional parameter for family choice

--- BabySitterKata/BabySitter.cs
+++ BabySitterKata/BabySitter.cs
@@ -4,9 +4,8 @@ namespace BabySitterKata
 {
     public class BabySitter
     {
-        public int CalculateNightlyCharge(string clockInTime, string clockOutTime)
+        public int CalculateNightlyCharge(string clockInTime, string clockOutTime, string Family)
         {
-            
             return 0;
         }
     }
--- BabySitterKata/Tests/BabySitterTest.cs
+++ BabySitterKata/Tests/BabySitterTest.cs
@@ -8,12 +8,16 @@ namespace BabySitterKata.Tests
         [Test()]
         public void WhenTheBabySitterCalculatesNightlyChargeItReturnsANumber()
         {
+            //Arrange
             BabySitter babySitter = new BabySitter();
             var clockInTime = string.Empty;
             var clockOutTime = string.Empty;
+            var family = string.Empty;
 
-            var earnings = babySitter.CalculateNightlyCharge(clockInTime, clockOutTime);
+            //Act
+            var earnings = babySitter.CalculateNightlyCharge(clockInTime, clockOutTime, family);
 
+            //Assert
             Assert.IsNotNull(earnings);
         }
     }
