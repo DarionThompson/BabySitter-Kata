38c51c2b0e91dcc96f566ed8d1ae6d0c583a3928
Darion Thompson <user@Darions-iMac.local>
2018-12-16 04:30:45 -0500

    Second Baby Sitter Test: This Test Covers Clock In time Validation before 5pm, Returns a validation message if this condition is met.

--- BabySitterKata/BabySitter.cs
+++ BabySitterKata/BabySitter.cs
@@ -4,9 +4,13 @@ namespace BabySitterKata
 {
     public class BabySitter
     {
-        public int CalculateNightlyCharge(string clockInTime, string clockOutTime, string Family)
+        public string CalculateNightlyCharge(string clockInTime, string clockOutTime, string Family)
         {
-            return 0;
+            if(DateTime.Parse(clockInTime) < DateTime.Parse("5PM")){
+                return "You Cannot clock In Before 5PM";
+            }
+
+            return "0";
         }
     }
 }
--- BabySitterKata/Tests/BabySitterTest.cs
+++ BabySitterKata/Tests/BabySitterTest.cs
@@ -10,8 +10,8 @@ namespace BabySitterKata.Tests
         {
             //Arrange
             BabySitter babySitter = new BabySitter();
-            var clockInTime = string.Empty;
-            var clockOutTime = string.Empty;
+            var clockInTime = "5PM";
+            var clockOutTime = "6";
             var family = string.Empty;
 
             //Act
@@ -19,6 +19,24 @@ namespace BabySitterKata.Tests
 
             //Assert
             Assert.IsNotNull(earnings);
+        }
+
+        [Test()]
+        public void IfTheclockInTimeStartsEarlilerThan5PMAnErrorValidationMessageIsReturned()
+        {
+            //Arrange
+            BabySitter babySitter = new BabySitter();
+            var clockInTime = "4PM";
+            var clockOutTime = string.Empty;
+            var family = string.Empty;
+
+            var earlyClockInTimeMessage = "You Cannot clock In Before 5PM";
+
+            //Act
+            var earnings = babySitter.CalculateNightlyCharge(clockInTime, clockOutTime, family);
+
+            //Assert
+            Assert.AreEqual(earnings, earlyClockInTimeMessage);
         }
     }
 }
