6f6762822225975ab8cd15b2170ee352175f8227
Darion Thompson <user@Darions-iMac.local>
2018-12-16 03:05:19 -0500

    added project and file structure to start with

---
+++ BabySitterKata/BabySitter.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace BabySitterKata
+{
+    class BabySitter
+    {
+        
+    }
+}
---
+++ BabySitterKata/Tests/BabySitterTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace BabySitterKata.Tests
+{
+    [TestFixture()]
+    public class BabySitterTest
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
