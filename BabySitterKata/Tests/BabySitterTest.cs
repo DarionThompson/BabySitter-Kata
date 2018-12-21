ad3318a646c9eccdebd9a5f242bec20e86879af2
Darion Thompson <darionthompson1@aol.com>
2018-12-20 23:52:08 -0500

    added unit test : Validates that if a baby sitter works from 5pm to 4am for family B that a value of 140 is returned

--- BabySitterKata/Tests/BabySitterTest.cs
+++ BabySitterKata/Tests/BabySitterTest.cs
@@ -5,109 +5,116 @@ using NUnit.Framework;
 namespace BabySitterKata.Tests
 {
     [TestFixture()]
-	public class BabySitterTest
-	{
+    public class BabySitterTest
+    {
 
-		private string _clockInTime;
+        private string _clockInTime;
 
-		private string _clockOutTime;
+        private string _clockOutTime;
 
-		private Family _familyChoiceA;
+        private Family _familyChoiceA;
 
         private Family _familyChoiceB;
 
-		private BabySitter _babySitter;
+        private BabySitter _babySitter;
 
-		[SetUp]
-		public void SetUp()
-		{
-			_clockInTime = "5PM";
+        [SetUp]
+        public void SetUp()
+        {
+            _clockInTime = "5PM";
 
-			_clockOutTime = "6PM";
+            _clockOutTime = "6PM";
 
-			_babySitter = new BabySitter();
+            _babySitter = new BabySitter();
 
-			_familyChoiceA = new FamilyA();
+            _familyChoiceA = new FamilyA();
 
             _familyChoiceB = new FamilyB();
-		}
+        }
 
-		[Test()]
-		public void IfTheclockInTimeStartsEarlilerThan5PMAnErrorValidationMessageIsReturned()
-		{
-			//Arrange
-			_clockInTime = "4PM";
-			var earlyClockInTimeMessage = "You cannout work before 5PM";
+        [Test()]
+        public void IfTheclockInTimeStartsEarlilerThan5PMAnErrorValidationMessageIsReturned()
+        {
+            //Arrange
+            _clockInTime = "4PM";
+            var earlyClockInTimeMessage = "You cannout work before 5PM";
 
-			//Act
-			var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
+            //Act
+            var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
 
-			//Assert
-			Assert.Contains(earlyClockInTimeMessage, earnings);
-		}
+            //Assert
+            Assert.Contains(earlyClockInTimeMessage, earnings);
+        }
 
-		[Test()]
-		public void IfTheBabySitterDoesNotSelectAFamilyAnErrorValidationMessageIsReturned()
-		{
-			//Arrange
-			var selectAFamilyMessage = "You Must Select A Family";
+        [Test()]
+        public void IfTheBabySitterDoesNotSelectAFamilyAnErrorValidationMessageIsReturned()
+        {
+            //Arrange
+            var selectAFamilyMessage = "You Must Select A Family";
 
-			//Act
-			var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, null).ToList();
+            //Act
+            var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, null).ToList();
 
-			//Assert
-			Assert.Contains(selectAFamilyMessage, earnings);
-		}
+            //Assert
+            Assert.Contains(selectAFamilyMessage, earnings);
+        }
 
 
-		[Test()]
-		public void IfTheclockoutTimeIsLaterThan4AMAnErrorValidationMessageIsReturned()
-		{
-			//Arrange
-			_clockOutTime = "5AM";
-			var lateClockOutTimeMessage = "You cannout work past 4AM";
+        [Test()]
+        public void IfTheclockoutTimeIsLaterThan4AMAnErrorValidationMessageIsReturned()
+        {
+            //Arrange
+            _clockOutTime = "5AM";
+            var lateClockOutTimeMessage = "You cannout work past 4AM";
 
-			//Act
-			var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
+            //Act
+            var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
 
-			//Assert
-			Assert.Contains(lateClockOutTimeMessage, earnings);
-		}
+            //Assert
+            Assert.Contains(lateClockOutTimeMessage, earnings);
+        }
 
-		[Test()]
-		public void IfTheclockoutTimeIsBeforeThanClockInTimeAnErrorValidationMessageIsReturned()
-		{
-			//Arrange
-			_clockInTime = "8PM";
-			_clockOutTime = "6PM";
-			var errorClockOutTimeMessage = "Your end time cannot be before your start time";
+        [Test()]
+        public void IfTheclockoutTimeIsBeforeThanClockInTimeAnErrorValidationMessageIsReturned()
+        {
+            //Arrange
+            _clockInTime = "8PM";
+            _clockOutTime = "6PM";
+            var errorClockOutTimeMessage = "Your end time cannot be before your start time";
 
-			//Act
-			var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
+            //Act
+            var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
 
-			//Assert
-			Assert.Contains(errorClockOutTimeMessage, earnings);
-		}
+            //Assert
+            Assert.Contains(errorClockOutTimeMessage, earnings);
+        }
 
-        [Test()]         public void IfTheBabySitterDoesNotSelectAClockInTimeASelectClockInTimeErrorValidationmessageIsReturned()         {             //Arrange             _clockInTime = string.Empty;
-            var selectAClockInTimeMessage = "You must select a start time";             //Act
-           var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();           
-           //Assert            Assert.Contains(selectAClockInTimeMessage, earnings);         }
+        [Test()]         public void IfTheBabySitterDoesNotSelectAClockInTimeASelectClockInTimeErrorValidationmessageIsReturned()         {
+            //Arrange
+            _clockInTime = string.Empty;
 
-		[Test()]
-		public void IfTheBabySitterDoesNotSelectAClockOutTimeASelectClockOutTimeErrorValidationmessageIsReturned()
-		{
-			//Arrange
+            var selectAClockInTimeMessage = "You must select a start time";
+
+            //Act
+            var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
+
+            //Assert
+            Assert.Contains(selectAClockInTimeMessage, earnings);         }
+
+        [Test()]
+        public void IfTheBabySitterDoesNotSelectAClockOutTimeASelectClockOutTimeErrorValidationmessageIsReturned()
+        {
+            //Arrange
             _clockOutTime = string.Empty;
 
-			var selectAClockOutTimeMessage = "You must select an end time";
+            var selectAClockOutTimeMessage = "You must select an end time";
 
-			//Act
-			var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
+            //Act
+            var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
 
-			//Assert
-			Assert.Contains(selectAClockOutTimeMessage, earnings);
-		}
+            //Assert
+            Assert.Contains(selectAClockOutTimeMessage, earnings);
+        }
 
         [Test()]
         public void IfTheBabySitterSelectsFamilyAAndWorksBetween5PMAnd11PMAValueOf90IsReturned()
@@ -157,6 +164,20 @@ namespace BabySitterKata.Tests
             //Arrange
             var expectedPay = "76";
             _clockOutTime = "12AM";
+
+            //Act
+            var actualPay = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceB).ToList();
+
+            //Assert
+            Assert.Contains(expectedPay, actualPay);
+        }
+
+        [Test()]
+        public void IfTheBabySitterSelectsFamilyBAndWorksBetween5PMAnd4AMAValueOf140IsReturned()
+        {
+            //Arrange
+            var expectedPay = "140";
+            _clockOutTime = "4AM";
 
             //Act
             var actualPay = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceB).ToList();
